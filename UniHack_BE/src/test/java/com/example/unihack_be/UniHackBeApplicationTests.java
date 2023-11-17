506ec381c4fe2e8917d27eed54264d2907aeda49
Bran Alexandru <bran.alexandru.2002@gmail.com>
2023-11-17 14:11:29 +0200

    Project Setup

---
+++ UniHack_BE/src/test/java/com/example/unihack_be/UniHackBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unihack_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniHackBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
